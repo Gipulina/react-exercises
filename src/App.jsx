e29ef29f9b843f08bea3202627f3cb10b50d2da6
gipulina90 <georgi_palavecino@hotmail.com>
2023-04-28 23:05:26 -0300

    05 useState with current State and newState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 // import ErrorExample from './tutorial/01-useState/starter/01-error-example';
 // import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
 // import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
-import UseStateObject from "./tutorial/01-useState/starter/04-useState-object";
+// import UseStateObject from "./tutorial/01-useState/starter/04-useState-object";
+import UseStateGotcha from  "./tutorial/01-useState/starter/05-useState-gotcha";
 
 function App() {
   return (
     <div className='container'>
       <h2>React Excersices</h2>
-      <UseStateObject/>
+      <UseStateGotcha/>
     </div>
   );
 }
--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const[value, setValue] = useState(0);
+
+  const handleClick = () => { 
+    // setValue(value + 1);
+    //This way, the current variable is taking the same value of the value variable, instead of showing one more number.
+    setValue((currentState)=>{
+      const newState = currentState + 1
+      console.log(currentState);
+      return newState;
+    } );
+  };
+  return (
+    <>
+      <h2>{value}</h2>
+      <button className="btn"type="button" onClick={handleClick}>Increase</button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
