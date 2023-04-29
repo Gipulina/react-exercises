099eda6e783e503309761d27edf17b624e7a8785
gipulina90 <georgi_palavecino@hotmail.com>
2023-04-28 22:42:36 -0300

    Button to change person information

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import ErrorExample from './tutorial/01-useState/starter/01-error-example';
 // import UseStateBasics from "./tutorial/01-useState/starter/02-useState-basics";
-import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
+// import UseStateArray from "./tutorial/01-useState/starter/03-useState-array";
+import UseStateObject from "./tutorial/01-useState/starter/04-useState-object";
 
 function App() {
   return (
     <div className='container'>
       <h2>React Excersices</h2>
-      <UseStateArray/>
+      <UseStateObject/>
     </div>
   );
 }
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+  //Aca ingreso la información pero como objeto
+  name:'peter',
+  age: 24,
+  hobby:'read books'
+  })
+
+  // const [name, setName] = useState("peter");
+  // const [age, setAge] = useState(25);
+  // const [hobbie, setHobbie] = useState("read books");
+  const displayPerson = () =>{
+    setPerson({...person, name:'susan'})
+    // setName('Jhon')
+    // setAge(23)
+    // setHobbie('Painting')
+  }
+  const displayPersonTwo = () => {
+    setPerson({name:'jhon', age: 25, hobby:'painting'})
+  }
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoys : </h4>
+      <h3>{person.hobbie}</h3>
+      <button className="btn" onClick={displayPerson}>Next Person</button>
+      <button className="btn" onClick={displayPersonTwo}>Next Person</button>
+    </>
+  );
 };
 
 export default UseStateObject;
